4823e56eadfeaa4d88f4d330ea15c08fb285b6ae
데이터엔지니어링-이지섭 <user@Users-MacBook-Pro.local>
2025-12-04 09:52:51 +0900

    first commit

---
+++ app/services/db.py
@@ -0,0 +1,12 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_engine():
+    db_url = (
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    )
+    return create_engine(db_url)
